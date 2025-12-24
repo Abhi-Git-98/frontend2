c99660202fc24f71c3864eae92f9ff547078bd5c
Abhi-Git-98 <abhitambe998@gmail.com>
2025-12-24 09:47:09 +0530

    Update News.jsx
    updated marquee notifications

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -50,7 +50,7 @@ export default function NewsTicker() {
       {show && (
         <div className="news-ticker" ref={tickerRef}>
             <div className="news-ticker-track">
-                <span>🔴 Registrations are open 🔴 Accomodation details will be updated soon 🔴  </span>
+                <span>🔴Poster presentation abstract submission last date is 31<sup>st</sup> December 2025 🔴 Registrations are open 🔴 Accomodation details will be updated soon 🔴  </span>
             </div>
         </div>
       )}
