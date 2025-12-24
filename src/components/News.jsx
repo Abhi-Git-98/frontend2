65339c03328a571c447b59bb43e8ef5fd44c608e
chaitanya2611 <115576523+chaitanya2611@users.noreply.github.com>
2025-12-24 09:20:44 +0530

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -50,7 +50,7 @@ export default function NewsTicker() {
       {show && (
         <div className="news-ticker" ref={tickerRef}>
             <div className="news-ticker-track">
-                <span>🔴 Registrations are open 🔴 Accomodation status will be updated soon 🔴  </span>
+                <span>🔴 Registrations are open 🔴 Accomodation details will be updated soon 🔴  </span>
             </div>
         </div>
       )}
