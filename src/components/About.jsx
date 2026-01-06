d73874c7723eab5acbb01bc10629a555647f601b
Abhi-Git-98 <abhitambe998@gmail.com>
2026-01-06 13:50:56 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -333,6 +333,9 @@ export default function AboutPage() {
         <div className="event-contact-container mt-5">
           <h2 className="event-contact-title">Event Related Queries?</h2>
           <p className="event-contact-sub">Don’t Overthink.</p>
+          <p className="event-contact-sub">Join the Community for more updates👇</p>
+          <a href="https://chat.whatsapp.com/EKGdRB2F6DkBQCf8qtC7hE">Whatsapp link</a>
+          <p className="event-contact-sub">Note: Before joining the group make sure to enroll and register yourselves for the events.</p>
 
           <div className="event-contact-cards">
             <div className="event-contact-card">
