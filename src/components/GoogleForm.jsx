5cdd7fce1fff809ecdb56deb56df02538e49819d
Abhi-Git-98 <abhitambe998@gmail.com>
2026-01-06 20:55:05 +0530

    Update GoogleForm.jsx

--- src/components/GoogleForm.jsx
+++ src/components/GoogleForm.jsx
@@ -4,8 +4,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Form() {
   return (
     <>
-      <h4 style={{color: "white"}}>If Google form is not visible, click
-      <a href="https://docs.google.com/forms/d/e/1FAIpQLSc6VAXb7Fz3GCzU-cL9NRCldbNfQ3-_EWGBGUnGVJkbcRloRQ/closedform?embedded=true"> link</a></h4>
+      <h4 style={{color: "white"}}>If Google form is not visible, click 
+      <a href="https://docs.google.com/forms/d/e/1FAIpQLSc6VAXb7Fz3GCzU-cL9NRCldbNfQ3-_EWGBGUnGVJkbcRloRQ/closedform?embedded=true">link</a></h4>
       <iframe
         title="Google Form"
         src="https://docs.google.com/forms/d/e/1FAIpQLSc6VAXb7Fz3GCzU-cL9NRCldbNfQ3-_EWGBGUnGVJkbcRloRQ/closedform?embedded=true"
