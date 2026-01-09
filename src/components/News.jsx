7332b32b176ccf8f76c90398e159e463646a2405
Abhi-Git-98 <abhitambe998@gmail.com>
2026-01-09 17:53:22 +0530

    Update News.jsx

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import "../css/news.css";
 import biofusabsreg from "./BIOFUSION_POSTER_PRESENTATION_COMPETITION.pdf";
-import hostelaccomo from "./Hostel_Accomodation_Genvision_2025-26.pdf";
 
 export default function NewsTicker() {
   const [show, setShow] = useState(false);
@@ -68,7 +67,6 @@ export default function NewsTicker() {
               ] 🔴 Registrations are open 🔴 Accomodation for outstation
               students are full 🔴 After payment verification login link and
               credentials will be shared on your registered email. 🔴 <b>Register for Events using your email id only.</b>{" "}
-              🔴 Details for Accomodation [<a href = {hostelaccomo} download>Click Here</a>] 🔴
             </span>
           </div>
         </div>
